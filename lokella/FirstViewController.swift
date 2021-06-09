1539d553731e2d007042754f120c141697544110
taskinkemal <kemaltaskin@yahoo.com>
2021-06-09 21:13:50 +0200

    Initial Commit

---
+++ lokella/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  lokella
+//
+//  Created by Kemal Taskin on 09.06.21.
+//  Copyright © 2021 Kelpersegg. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
