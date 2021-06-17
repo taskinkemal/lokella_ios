a02f0adc9fbc5cbb94ac23b7d3e0b560f68e88ec
kemaltaskin <kemaltaskin@yahoo.com>
2021-06-17 23:24:01 +0200

    .

--- lokella/models/CatalogAdditive.swift
+++ lokella/models/CatalogAdditive.swift
@@ -8,38 +8,6 @@
 
 import Foundation
 
-class CatalogAdditive : BaseModel, Codable
+class CatalogAdditive : CatalogAllergyAdditive
 {
-    @objc public dynamic var Id: Int16 = 0
-    @objc public dynamic var ShortName: String = ""
-    @objc public dynamic var Name: String = ""
-    
-    private enum CodingKeys: String, CodingKey {
-        case Id = "Id"
-        case ShortName = "ShortName"
-        case Name = "Name"
-    }
-    
-    required convenience init(from decoder: Decoder) throws {
-        self.init()
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        Id = try container.decode(Int16.self, forKey: .Id)
-        ShortName = try container.decode(String.self, forKey: .ShortName)
-        Name = try container.decode(String.self, forKey: .Name)
-    }
-    
-    convenience init(_ id: Int16, _ shortName: String, _ name: String) {
-        self.init()
-        self.Id = id
-        self.ShortName = shortName
-        self.Name = name
-    }
-    
-    func toJSON() -> NSDictionary {
-        return [
-            "Id": self.Id,
-            "ShortName": self.ShortName,
-            "Name": self.Name
-        ]
-    }
 }
--- lokella/models/CatalogAllergy.swift
+++ lokella/models/CatalogAllergy.swift
@@ -8,33 +8,7 @@
 
 import Foundation
 
-class CatalogAllergy : BaseModel, Codable
+class CatalogAllergy : CatalogAllergyAdditive
 {
-    @objc public dynamic var Id: Int16 = 0
-    @objc public dynamic var Name: String = ""
     
-    private enum CodingKeys: String, CodingKey {
-        case Id = "Id"
-        case Name = "Name"
-    }
-    
-    required convenience init(from decoder: Decoder) throws {
-        self.init()
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        Id = try container.decode(Int16.self, forKey: .Id)
-        Name = try container.decode(String.self, forKey: .Name)
-    }
-    
-    convenience init(_ id: Int16, _ name: String) {
-        self.init()
-        self.Id = id
-        self.Name = name
-    }
-    
-    func toJSON() -> NSDictionary {
-        return [
-            "Id": self.Id,
-            "Name": self.Name
-        ]
-    }
 }
